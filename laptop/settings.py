11e84f2382dec27018acf25241742cebcf668f32
Bektur <bekturkochorbaev64@gmail.com>
2025-07-26 19:51:11 +0600

    update settings.py

--- laptop/settings.py
+++ laptop/settings.py
@@ -132,10 +132,8 @@ REST_FRAMEWORK = {
 
 }
 
-
-
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=17),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=55),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,
